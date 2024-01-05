0942a9271c752117652a2b7330d7edecbc34919d
tu-hm <tu.hm@teko.vn>
2024-01-05 19:55:49 +0700

    fix error log

--- App.tsx
+++ App.tsx
@@ -4,8 +4,9 @@ import { Colors } from "./constant/Color";
 import StackNavigator from "./navigation/StackNavigator";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-
+import { LogBox } from "react-native";
 export default function App() {
+  LogBox.ignoreAllLogs();
   return (
     <Provider store={store}>
       <StatusBar
